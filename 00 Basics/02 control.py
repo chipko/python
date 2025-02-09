79d0afb11fc2500ff8aee66b993f7b8d25ebf438
chipko <chipko.android@gmail.com>
2025-02-09 20:39:04 +0000

    added more info

--- 00 Basics/02 control.py
+++ 00 Basics/02 control.py
@@ -6,3 +6,21 @@ import os
 os.system("cls")
 
 print("Control flow / loops")
+
+for x in range(0,10):
+    print(x, ' ', end="")
+
+print('\n')
+# break will exit the loop
+for x in range(0,11):
+    if x == 9:
+        break
+    print(x, ' ', end="")
+
+print("\n")
+# continue will skip over that iteration
+for x in range(0,11):
+    if x == 9:
+        continue
+    print(x, ' ', end="")
+
